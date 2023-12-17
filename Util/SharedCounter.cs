cf0bcf93e9209de877d7556abaf7d16d86fd4f68
mhb164 <mhb164@gmail.com>
2023-12-17 17:32:24 +0100

    Add Util project

---
+++ Util/SharedCounter.cs
@@ -0,0 +1,12 @@
+﻿namespace Util
+{
+    public class SharedCounter
+    {
+        public static int CurrentCount { get; private set; } = 0;
+
+        public static void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
